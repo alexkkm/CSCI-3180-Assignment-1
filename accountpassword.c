79cdd2d824209ff8a8be91f8c56fa08691a6ffe0
alexkkm <alexkong0222@gmail.com>
2022-01-21 16:50:53 +0800

    check AccPw in accountpassword.c

--- accountpassword.c
+++ accountpassword.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 // Copy the (length) characters form (target) string form (poisition)th position
 char *str_slice(char *target, int position, int length)
@@ -14,24 +13,65 @@ char *str_slice(char *target, int position, int length)
     return temp;
 }
 
-void test()
+// Read all lines into the buff
+void ReadAllLine()
 {
     // READ File
     char buff[10000] = "null";
-    char input[58];
-    int found = 0;
+    char input[59];
 
     // open the master.txt with
     FILE *fp = fopen("./master.txt", "rt");
-    fgets(buff, 10000, fp);
-    strcpy(input, buff);
-    fclose(fp);
-    fp = fopen("./master.txt", "rt");
-    strcpy(input, buff);
-    fclose(fp);
-    printf("Account Name:%s\n", str_slice(input, 0, 20));
+    // fgets for lineNum times so that we can read the (lineNum)th lines
+    while (fgets(buff, 60, fp) != NULL)
+    {
+        strcpy(input, buff);
+        printf("Input: %s\n", input);
+    }
 }
 
+// Return 0 if account password pair not found, return 1 if found
+int CheckAccountPassword(char *account, char *password)
+{
+    // READ File
+    char buff[60] = "null";
+    char input[59];
+    int valid = 0;
+    char correctAccount[17];
+    char correctPassword[7];
+
+    // open the master.txt with
+    FILE *fp = fopen("./master.txt", "rt");
+    // fgets for lineNum times so that we can read the (lineNum)th lines
+    while (fgets(buff, 60, fp) != NULL)
+    {
+        strcpy(input, buff);
+        strcpy(correctAccount, str_slice(input, 20, 16));
+        printf("Input Account:%s\n", account);
+        printf("Correct Account:%s\n", correctAccount);
+        // Cut the account number and check
+        if (strcmp(account, correctAccount) == 0)
+        {
+            printf("Account Found!\n");
+            // fclose(fp);
+            strcpy(correctPassword, str_slice(buff, 36, 6));
+            printf("Correct Password:%s\n", correctPassword);
+            if (strcmp(password, correctPassword) == 0)
+            {
+                printf("Account and Password Correct\n");
+                valid = 1;
+            }
+            else
+            {
+                printf("Password Incorrect!\n");
+            }
+        }
+        printf("end single while loop\n\n");
+    }
+    return valid;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
 // Read the Account Name
 void ReadAccountName(int lineNum)
 {
@@ -92,8 +132,10 @@ void ReadAccountPassword(int lineNum)
     printf("Account Password:%s\n", str_slice(input, 36, 6));
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+
 int main()
 {
-    printf("Testing:\n");
-    ReadAccountName(1);
+    printf("Result:%d", CheckAccountPassword("1234567890123456", "123457")); // password incorrect
+    return 0;
 }
